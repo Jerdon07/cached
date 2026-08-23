8ab08ec53ac86a08eb74e86c1656c85e405d6c68
Jerdon07 <jdlitaoen@gmail.com>
2026-08-23 11:19:34 +0800

    FEAT: Enhance PurchaseOrderInfolist with detailed sections and improved labels

--- app/Filament/Resources/PurchaseOrders/Schemas/PurchaseOrderInfolist.php
+++ app/Filament/Resources/PurchaseOrders/Schemas/PurchaseOrderInfolist.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\PurchaseOrders\Schemas;
 
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class PurchaseOrderInfolist
@@ -10,33 +11,62 @@ class PurchaseOrderInfolist
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(12)
             ->components([
-                TextEntry::make('supplier.id')
-                    ->label('Supplier'),
-                TextEntry::make('created_by')
-                    ->numeric()
-                    ->placeholder('-'),
-                TextEntry::make('approved_by')
-                    ->numeric()
-                    ->placeholder('-'),
-                TextEntry::make('order_date')
-                    ->date(),
-                TextEntry::make('expected_delivery_date')
-                    ->date(),
-                TextEntry::make('status')
-                    ->badge(),
-                TextEntry::make('notes')
-                    ->placeholder('-')
-                    ->columnSpanFull(),
-                TextEntry::make('approved_at')
-                    ->date()
-                    ->placeholder('-'),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
+                Section::make('Details')
+                    ->columnSpan(function ($record) {
+                        if ($record->approved_by) return 5;
+                        else return 'full';
+                    })
+                    ->schema([
+                        TextEntry::make('createdBy.name')
+                            ->numeric()
+                            ->placeholder('-')
+                            ->belowContent(fn ($record) => "| $record->notes")
+                            ->inlineLabel(fn ($record) => !$record->approved_by),
+                        TextEntry::make('supplier.company_name')
+                            ->inlineLabel()
+                            ->label('Supplier: '),
+                        TextEntry::make('status')
+                            ->inlineLabel()
+                            ->label('Status: ')
+                            ->badge(),
+                    ]),
+                
+                Section::make('Order Information')
+                    ->hidden(fn ($record) => !$record?->approved_by)
+                    ->columnSpan(7)
+                    ->inlineLabel()
+                    ->schema([
+                        TextEntry::make('approvedBy.name')
+                            ->label('Approved by:')
+                            ->numeric()
+                            ->hidden(fn ($record) => !$record?->approved_by),
+                        TextEntry::make('order_date')
+                            ->label('Ordered Date:')
+                            ->date()
+                            ->hidden(fn ($record) => !$record?->approved_by),
+                        TextEntry::make('expected_delivery_date')
+                            ->label('Expected Delivery Date:')
+                            ->date()
+                            ->hidden(fn ($record) => !$record?->approved_by),
+                        TextEntry::make('approved_at')
+                            ->label('Approved at:')
+                            ->date()
+                            ->hidden(fn ($record) => !$record?->approved_by),
+                    ]),
+
+                Section::make()
+                    ->columnSpanFull()
+                    ->columns(2)
+                    ->schema([
+                        TextEntry::make('created_at')
+                            ->dateTime()
+                            ->placeholder('-'),
+                        TextEntry::make('updated_at')
+                            ->dateTime()
+                            ->placeholder('-'),
+                    ])
             ]);
     }
 }
