1cfa290c6b5d43c84d6d55b37c6eec29243d3925
Jerdon07 <jdlitaoen@gmail.com>
2026-08-23 15:39:42 +0800

    FEAT: Add header actions to ListPurchaseOrders for creating new purchase orders

--- app/Filament/Resources/PurchaseOrders/Pages/ListPurchaseOrders.php
+++ app/Filament/Resources/PurchaseOrders/Pages/ListPurchaseOrders.php
@@ -10,4 +10,11 @@ use Filament\Resources\Pages\ListRecords;
 class ListPurchaseOrders extends ListRecords
 {
     protected static string $resource = PurchaseOrderResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
