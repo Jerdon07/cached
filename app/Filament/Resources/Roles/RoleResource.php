b8e22c3f41f657c05eed9dc3c0fcd404937d41fb
Jerdon07 <jdlitaoen@gmail.com>
2026-08-29 15:44:25 +0800

    FEAT: Create users relation manager to role resource

--- app/Filament/Resources/Roles/RoleResource.php
+++ app/Filament/Resources/Roles/RoleResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\Roles\Pages\CreateRole;
 use App\Filament\Resources\Roles\Pages\EditRole;
 use App\Filament\Resources\Roles\Pages\ListRoles;
 use App\Filament\Resources\Roles\Pages\ViewRole;
+use App\Filament\Resources\Roles\RelationManagers\UsersRelationManager;
 use App\Filament\Resources\Roles\Schemas\RoleForm;
 use App\Filament\Resources\Roles\Schemas\RoleInfolist;
 use App\Filament\Resources\Roles\Tables\RolesTable;
@@ -43,7 +44,7 @@ class RoleResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            UsersRelationManager::class,
         ];
     }
 
