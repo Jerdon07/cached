3782d57d87ce727d564626f54c9200bbc391ef32
Jerdon07 <jdlitaoen@gmail.com>
2026-08-29 20:49:31 +0800

    FIX: Make the stock movement as view any policy only

--- app/Policies/StockMovementPolicy.php
+++ app/Policies/StockMovementPolicy.php
@@ -10,36 +10,6 @@ class StockMovementPolicy
 {
     public function viewAny(User $user): bool
     {
-        return false;
-    }
-
-    public function view(User $user, StockMovement $stockMovement): bool
-    {
-        return $user->hasPermissionTo('view_stock_movements');
-    }
-
-    public function create(User $user): bool
-    {
-        return false;
-    }
-
-    public function update(User $user, StockMovement $stockMovement): bool
-    {
-        return false;
-    }
-
-    public function delete(User $user, StockMovement $stockMovement): bool
-    {
-        return false;
-    }
-
-    public function restore(User $user, StockMovement $stockMovement): bool
-    {
-        return false;
-    }
-
-    public function forceDelete(User $user, StockMovement $stockMovement): bool
-    {
-        return false;
+        return $user->hasPermissionTo('view_any_stock_movements');
     }
 }
