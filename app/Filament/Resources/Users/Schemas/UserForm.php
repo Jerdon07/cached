3c9634d8f2fa1bd225a121f2e2bd3b097da4c2dd
Jerdon07 <jdlitaoen@gmail.com>
2026-08-29 15:38:41 +0800

    FEAT: Create resource for user

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use App\Models\Role;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->string()
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->maxLength(255),
+                CheckboxList::make('roles')
+                    ->relationship('roles', 'name')
+                    ->options(fn () => Role::all()->pluck('name')),
+            ]);
+    }
+}
