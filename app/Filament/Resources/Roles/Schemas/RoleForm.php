c1d60394453e472689e528963f9ff9cd9da14d8d
Jerdon07 <jdlitaoen@gmail.com>
2026-08-31 12:26:40 +0800

    FEAT: Add form components for role name, description, and permissions

--- app/Filament/Resources/Roles/Schemas/RoleForm.php
+++ app/Filament/Resources/Roles/Schemas/RoleForm.php
@@ -2,7 +2,13 @@
 
 namespace App\Filament\Resources\Roles\Schemas;
 
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class RoleForm
 {
@@ -10,7 +16,13 @@ class RoleForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name'),
+                Textarea::make('description'),
+                Select::make('permissions')
+                    ->getOptionLabelFromRecordUsing(fn (Model $record): string => Str::headline($record->name))
+                    ->relationship('permissions', 'name')
+                    ->columnSpanFull()
+                    ->multiple()
             ]);
     }
 }
